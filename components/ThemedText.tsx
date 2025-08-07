83fb530227cc196b35e7088a31738b4f54d6fc40
SWift Admin <swiftaicompany@gmail.com>
2025-08-07 12:09:53 +0200

    refactor: remove unused components and hooks to streamline codebase

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'link';
 };
 
 export function ThemedText({
@@ -24,7 +24,6 @@ export function ThemedText({
         type === 'default' ? styles.default : undefined,
         type === 'title' ? styles.title : undefined,
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
         style,
       ]}
@@ -47,10 +46,6 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: 'bold',
     lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
   },
   link: {
     lineHeight: 30,
